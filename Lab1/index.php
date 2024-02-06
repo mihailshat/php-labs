13869cae88ce92b8785d48c6af24797c93b25d53
mihailshat <mihailshatt@gmail.com>
2024-02-06 14:58:07 +0300

    first commit

---
+++ Lab1/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+        <?php
+            // echo '<h1>Hello, PHP</h1>';
+            $a = 100;
+            $b = 'A';
+
+            // if ($a == 100) $a = 'ok';
+            //     else $a = 'no';
+
+            echo ($a == 100)? 'ok' : 'no';
+
+            
+            
+            // if ($a === $b) echo 'ok';
+            // else echo 'no';
+
+
+
+            // if($a < $b) echo 'ok';
+            // else echo 'no';
+            
+
+            // echo $a.'<br>';
+            // echo ++$a;
+            // echo $a .= $a ;
+
+
+            // define('pi', 3.14);
+
+            // echo $a+$b;
+            // $a = (int)$b;
+            // echo $a 
+            // echo "<h2> $a </h2>";
+            // $x = 3.4;
+            // $z = 5.8;
+            // echo '$x = '.floor($x).'$z = '.floor($z).'<br>';
+            // echo '$x = '.round($x).'$z = '.round($z).'<br>';
+            // echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
+            // print '2<sup>10='.pow(2,10);
+        ?>
+
+</body>
+</html>
