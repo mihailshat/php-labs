78e47ec7b9fe68f45230d970fff9c6ceffe658d0
mihailshat <mihailshatt@gmail.com>
2024-02-06 17:24:13 +0300

    commit

--- Lab1/index.php
+++ Lab1/index.php
@@ -13,6 +13,102 @@
         </h1>
         <h2>Задания для самостоятельной работы <br>
             <?php
+            // echo '<h1>Hello, PHP</h1>';
+            // $a = 100;
+            // $b = 'A';
+
+            // if ($a == 100) $a = 'ok';
+            //     else $a = 'no';
+
+            // echo ($a == 100)? 'ok' : 'no';
+
+            
+            
+            // if ($a === $b) echo 'ok';
+            // else echo 'no';
+
+
+
+            // if($a < $b) echo 'ok';
+            // else echo 'no';
+            
+
+            // echo $a.'<br>';
+            // echo ++$a;
+            // echo $a .= $a ;
+
+
+            // define('pi', 3.14);
+
+            // echo $a+$b;
+            // $a = (int)$b;
+            // echo $a 
+            // echo "<h2> $a </h2>";
+            // $x = 3.4;
+            // $z = 5.8;
+            // echo '$x = '.floor($x).'$z = '.floor($z).'<br>';
+            // echo '$x = '.round($x).'$z = '.round($z).'<br>';
+            // echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
+            // print '2<sup>10='.pow(2,10);
+
+
+
+
+            // $a = 1;
+            // $b = 3;
+            // $c = 5;
+            // switch($a + $b){
+            //     case 3:
+            //         $d = 'three';
+            //         echo $d.'<br>';
+            //         break;
+            //     case 4:
+            //         $d = 'four';
+            //         echo $d.'<br>';
+            //         break;
+            //     case 7:
+            //         $d = 'seven';
+            //         echo $d.'<br>';
+            //         break;
+            //     default: echo 'no<br>';
+            // }
+
+            // $i = 2;
+            // $summ = 0;
+            // switch($i){
+            //     case 1: $summ++;
+            //     case 2: $summ++;
+            //     case 3: $summ++;
+            //     case 4: $summ++;
+            //     case 5: $summ++;
+            // }
+            // echo $summ;
+            // $i = 0; $sum = 0;
+            // while($i < 5){
+            //     $sum += $i;
+            //     $i++;
+            // }
+            // echo $sum;
+
+            // do{ $sum += $i;
+            //     $i++;
+            // }while($i < 5);
+            // echo $sum.'<br>';
+
+
+
+            // $k = 0;
+            // $arr = array(3,6,1,23,6);
+            // for ($i = 0; $i < count($arr); $i++){
+            //     $arr [$i]--;
+            //     if (!$arr[$i]) continue;
+            //     $k++;
+            // }
+            // echo $k.'<br>';
+
+
+
+
             // $a = 27;
             // $b = 12;
             // echo round(sqrt($a**2 + $b**2),2).'<br>';
@@ -64,6 +160,7 @@
             // $f = (float)$f;
 
             // echo "Полученное значение $a {$b} {$c} {$d} {$g} {$f} ";
+            
             ?>
         </h2>
 </body>
