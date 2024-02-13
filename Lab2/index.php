21911a248f09c8d14fded825c3e4b8856ba6ed88
mihailshat <mihailshatt@gmail.com>
2024-02-13 15:02:58 +0300

    labs2

--- Lab2/index.php
+++ Lab2/index.php
@@ -8,7 +8,31 @@
 <body>
     <h1>
         <?php
-
+         $equation = 'x * 7 = 49';
+        $elements = explode(" ", $equation);
+        $operator = $elements[1];
+        $operand = $elements[2];
+        $result = $elements[4];
+        $x = 0;
+        switch ($operator) {
+            case '+':
+                $x = $result - $operand;
+                echo $x; break;
+            case '-':
+                $x = $result + $operand;
+                echo $x; break;
+            case '/':
+                $x = $result * $operand;
+                echo $x; break;
+            case '*':
+                $x = $result / $operand;
+                echo $x; break;
+            case '**':
+                $x = $result ** (1 / $operand);
+                echo $x; break;
+            default:
+                echo 'Оператор введен неверно';
+        };
 
 
 
