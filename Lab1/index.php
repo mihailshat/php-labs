58e1163618e4000da50f79459d3d475fac70c526
mihailshat <mihailshatt@gmail.com>
2024-02-13 14:51:11 +0300

    lab1,2

--- Lab1/index.php
+++ Lab1/index.php
@@ -161,6 +161,44 @@
 
             // echo "Полученное значение $a {$b} {$c} {$d} {$g} {$f} ";
             
+
+
+
+
+
+
+
+
+
+
+
+
+            // $arr = array(1,4,7,6);
+            // for ($i = 0; $i< count($arr); $i++){
+            //     echo $arr[$i].'<br>'
+            // }
+            // $arr2 = array(1,'4',7,6);
+            // $arr3[0] = 'summer';
+            // $arr3[1] = 'winter';
+            // $arr3[3] = 'automn';
+            // $arr3[8] = '8';
+            // for ($i = 0; $i< count($arr); $i++){
+            //     echo $arr3[$i].'<br>'
+            // }
+        
+            // foreach($arr3 as $key=>$value){
+            //     echo "$key => $value <br>";
+            // }
+        
+            // $str = 'X+2=4';
+            //     explode($str, '=');
+        
+            // $m1 = array(1,2);
+            // $m2 = array(3,4);
+            // $m3 = $m1 + $m2;
+            // var_dump($m3)
+        
+        
             ?>
         </h2>
 </body>
